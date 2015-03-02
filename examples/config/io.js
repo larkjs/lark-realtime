80e830d8f6d185a17749b39d66cf9755cc80cd64
mbu-se-vc <mbu-se-vc@mbu-se-vcdeiMac.local>
2015-03-02 12:05:00 +0800

    fix code, now users are supposed to set io configs

--- examples/config/io.js
+++ examples/config/io.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.io = {
-    directory: 'examples/io',
+    directory: 'io',
     type     : 'ws'
     //type     : 'socket.io'
 };
--- examples/index.js
+++ examples/index.js
@@ -1,7 +1,9 @@
 var realtime = require('..');
 var lark     = require('lark');
 
-var app = realtime(lark());
+var app = lark();
+
+realtime(app, app.config.io);
 
 app.run(8300, function(){
     console.log("Lark system listening at 8300...");   
